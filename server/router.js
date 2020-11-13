6a4e0133a54fc16d1c7e3928822935261201c6e8
Rushabh Mehta <rushabh.r.mehta@gmail.com>
2020-11-13 12:15:59 +0300

    FEAT: Backend up and running with game logic

---
+++ server/router.js
@@ -0,0 +1,6 @@
+const router = require('express').Router();
+const gameController = require('./controller/gameController');
+
+router.post('/userMove', gameController.userMove)
+
+module.exports = router;
